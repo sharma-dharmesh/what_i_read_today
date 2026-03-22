020b96c69b91da47175350b8742addebab1dbe37
Dharmesh Sharma <contact@dharmeshsharma.me>
2026-03-22 23:22:06 +0530

    Added Topics page and linked each topic with detailed entry

--- wirt/urls.py
+++ wirt/urls.py
@@ -6,6 +6,12 @@ app_name = 'wirt'
 
 urlpatterns = [
     # Home Page
-    path('', views.index, name='index'),
+    path('', views.index, name = 'index'),
+
+    # Topic Page
+    path('topics/', views.topics, name = 'topics'),
+
+    # Detail page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name = 'topic'),
 
 ]
