076a9eaef75f864805b91ad3d2547011eaf4e068
Dharmesh Sharma <contact@dharmeshsharma.me>
2026-03-24 23:47:35 +0530

    Added a feature to add entries from the the topic page

--- wirt/urls.py
+++ wirt/urls.py
@@ -16,4 +16,7 @@ urlpatterns = [
 
     # Page for adding a new Topic
     path('new_topic/', views.new_topic, name = 'new_topic'),
+
+    # Page for adding a new entry
+    path ('new_entry/<int:topic_id>/', views.new_entry, name = 'new_entry' )
 ]
--- wirt/views.py
+++ wirt/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 
 from . models import Topic
-from . forms import TopicForm
+from . forms import TopicForm, EntryForm
+
 
 def index(request):
     """The home page for wirt"""
@@ -37,3 +38,25 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'wirt_pages/new_topic.html', context)
 
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic"""
+    topic = Topic.objects.get(id = topic_id)
+
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data = request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit = False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('wirt:topic', topic_id = topic_id)
+        
+        # Display a blank or invalid form
+        context = {'topic': topic, 'form': form}
+        return render(request, 'wirt_pages/new_entry.html', context)
+    
